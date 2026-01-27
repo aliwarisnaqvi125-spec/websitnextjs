da4189f5325feffca7904df8b2c08e9fa5632287
aliwarisnaqvi125-spec <aliwarisnaqvi125@gmail.com>
2026-01-27 08:44:06 -0800

    upload sw.js file

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import ServiceWorkerRegister from "@/components/ServiceWorkerRegister";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,8 +21,7 @@ export default function RootLayout({ children }) {
         <Navbar />
         {children}
         <Footer />
-
-
+        <ServiceWorkerRegister />
       </body>
     </html>
   );
